63338ffa737828be3e0f8dce6dd4c1d2f194f186
Juan Cartagena <4355955+juancartagena@users.noreply.github.com>
2023-03-24 14:57:13 -0500

    Define QA environment

---
+++ qa/main.tf
@@ -0,0 +1,13 @@
+module "qa" {
+    source = "../modules/blog"
+
+    instance_type = "t3.micro"
+
+    environment = {
+        name = qa
+        network_prefix = "172.24"
+    }
+
+    asg_min_size = 1
+    asg_max_size = 1
+}
